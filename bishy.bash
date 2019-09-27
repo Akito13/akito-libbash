ed062d879bd24983b2d9adc7ef214012ed23e6b3
Akito <akito@noreply.example.org>
2019-09-27 03:41:32 +0200

    Added "mergeEmptyLines" + general improvements

--- bishy.bash
+++ bishy.bash
@@ -12,10 +12,21 @@ function checkPriv {
   fi;
 }
 
-function truncEmpty {
+function mergeEmptyLines {
+  ## Merge each set of
+  ## multiple consecutive 
+  ## empty lines into a single
+  ## empty line within the
+  ## provided text file.
+  local file="$1"
+  cat ${file} | \
+  sed -i '$!N; /^\(.*\)\n\1$/!P; D' ${file}
+}
+
+function truncEmptyLines {
   ## Remove redundant newlines at EOF.
   ## Leave only a single one.
-  local file=$1;
+  local file="$1";
   if [ -s ${file} ]; then
     while [[ $(tail -n 1 ${file}) == "" ]]; do
       truncate -cs -1 ${file};
@@ -26,9 +37,9 @@ function truncEmpty {
 }
 
 function rmDisused {
-  ## Remove string $2 within file $1.
-  local file=$1;
-  local entry=$2;
+  ## Remove string "$2" within file "$1".
+  local file="$1";
+  local entry="$2";
   while read -r line; do
     [[ ! $line =~ ${entry} ]] && echo "$line";
   done <${file} > NIYTyOOyTYIN;
@@ -38,7 +49,7 @@ function rmDisused {
 function checkIP {
   ## Checks given IP format.
   ## Soft checking; no IP class matching.
-  local ip=$1;
+  local ip="$1";
   if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]; then
     return 0;
   elif [[ $ip =~ ^[A-Fa-f0-9:]+ ]]; then
