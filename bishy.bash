d56d6edea8323c7e72f18444dc9529b0a257ab8e
Akito <the@akito.ooo>
2020-01-13 02:45:24 +0100

    Fancy

--- bishy.bash
+++ bishy.bash
@@ -17,31 +17,31 @@
 #########################################################################
 ## BASH library with common utils.
 
-####  Boilerplate of the Boilerplate
-# Coloured Echoes
-function red_echo      { echo -e "\033[31m$@\033[0m";   }
-function green_echo    { echo -e "\033[32m$@\033[0m";   }
-function yellow_echo   { echo -e "\033[33m$@\033[0m";   }
-function white_echo    { echo -e "\033[1;37m$@\033[0m"; }
-# Coloured Printfs
-function red_printf    { printf "\033[31m$@\033[0m";    }
-function green_printf  { printf "\033[32m$@\033[0m";    }
-function yellow_printf { printf "\033[33m$@\033[0m";    }
-function white_printf  { printf "\033[1;37m$@\033[0m";  }
-# Debugging Outputs
-function white_brackets { local args="$@"; white_printf "["; printf "${args}"; white_printf "]";  }
-function echoInfo  { local args="$@"; white_brackets $(green_printf "INFO") && echo " ${args}";   }
-function echoWarn  { local args="$@"; white_brackets $(yellow_printf "WARN") && echo " ${args}";  }
-function echoError { local args="$@"; white_brackets $(red_printf "ERROR") && echo " ${args}";    }
-# Silences commands' STDOUT as well as STDERR.
-function silence { local args="$@"; ${args} &>/dev/null; }
-# Check your privilege.
-function checkPriv { if [[ "$EUID" != 0 ]]; then echoError "Please run me as root."; exit 1; fi; }
-# Returns 0 if script is sourced, returns 1 if script is run in a subshell.
-function checkSrc { (return 0 2>/dev/null); if [[ "$?" == 0 ]]; then return 0; else return 1; fi; }
-# Prints directory the script is run from. Useful for local imports of BASH modules.
-function whereAmI { printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";  }
-####
+#################################   Boilerplate of the Boilerplate   #################################
+# Coloured Echoes                                                                                    #
+function red_echo      { echo -e "\033[31m$@\033[0m";   }                                            #
+function green_echo    { echo -e "\033[32m$@\033[0m";   }                                            #
+function yellow_echo   { echo -e "\033[33m$@\033[0m";   }                                            #
+function white_echo    { echo -e "\033[1;37m$@\033[0m"; }                                            #
+# Coloured Printfs                                                                                   #
+function red_printf    { printf "\033[31m$@\033[0m";    }                                            #
+function green_printf  { printf "\033[32m$@\033[0m";    }                                            #
+function yellow_printf { printf "\033[33m$@\033[0m";    }                                            #
+function white_printf  { printf "\033[1;37m$@\033[0m";  }                                            #
+# Debugging Outputs                                                                                  #
+function white_brackets { local args="$@"; white_printf "["; printf "${args}"; white_printf "]";  }  #
+function echoInfo  { local args="$@"; white_brackets $(green_printf "INFO") && echo " ${args}";   }  #
+function echoWarn  { local args="$@"; white_brackets $(yellow_printf "WARN") && echo " ${args}";  }  #
+function echoError { local args="$@"; white_brackets $(red_printf "ERROR") && echo " ${args}";    }  #
+# Silences commands' STDOUT as well as STDERR.                                                       #
+function silence { local args="$@"; ${args} &>/dev/null; }                                           #
+# Check your privilege.                                                                              #
+function checkPriv { if [[ "$EUID" != 0 ]]; then echoError "Please run me as root."; exit 1; fi;  }  #
+# Returns 0 if script is sourced, returns 1 if script is run in a subshell.                          #
+function checkSrc { (return 0 2>/dev/null); if [[ "$?" == 0 ]]; then return 0; else return 1; fi; }  #
+# Prints directory the script is run from. Useful for local imports of BASH modules.                 #
+function whereAmI { printf "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";   }  #
+######################################################################################################
 
 function mergeEmptyLines {
   ## Merge each set of
